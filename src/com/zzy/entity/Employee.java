90f8d70ede57a6bbd65156753aca3cd098b800f4
z1103476299 <17858506977@163.com>
2018-08-30 09:06:45 +0800

    ssh_employee

---
+++ src/com/zzy/entity/Employee.java
@@ -0,0 +1,92 @@
+package com.zzy.entity;
+
+import java.util.Date;
+
+/**
+ * 员工实体类
+ */
+public class Employee {
+    private Integer eid;
+    private String ename;
+    private String gender;
+    private Date birthday;
+    private Date joinDate;
+    private String eno;
+    private String username;
+    private String password;
+
+    //所属部门
+    private Department department;
+
+    public Integer getEid() {
+        return eid;
+    }
+
+    public void setEid(Integer eid) {
+        this.eid = eid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getEno() {
+        return eno;
+    }
+
+    public void setEno(String eno) {
+        this.eno = eno;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
